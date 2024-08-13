c621327111bc4eb0fcc5e676e22303a58e18bd50
ArvindKalia <137801580+ArvindKalia@users.noreply.github.com>
2024-08-12 23:04:43 -0400

    changes

---
+++ Backend/functions/app.js
@@ -0,0 +1,13 @@
+//app.js
+
+import express from "express"
+const serverless = require("serverless-http");
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("App is running..");
+});
+
+app.use("/.netlify/functions/app", router);
+module.exports.handler = serverless(app);
